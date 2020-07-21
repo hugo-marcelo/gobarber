eb4657f924fc6b1cc6e52cf53b5d0e73c41b41f2
hugo-marcelo <hugo.marcelo@lg.com.br>
2020-07-21 15:02:56 -0300

    chore: updated package json of web

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -1,49 +1,37 @@
 module.exports = {
-  "env": {
-      "es6": true,
-      "jest": true,
-      "browser": true,
+  env: {
+    es6: true,
+    jest: true,
+    browser: true,
   },
-  "extends": [
-      "airbnb",
-      "prettier",
-      "prettier/react"
-  ],
-  "globals": {
-      "Atomics": "readonly",
-      "SharedArrayBuffer": "readonly"
+  extends: ['airbnb', 'prettier', 'prettier/react'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
   },
-  "parser": "babel-eslint",
-  "parserOptions": {
-      "ecmaFeatures": {
-          "jsx": true
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier', 'react-hooks'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
+    'import/prefer-default-export': 'off',
+    'no-param-reassign': 'off',
+    camelcase: 'off',
+    'no-console': ['error', { allow: ['tron'] }],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+  settings: {
+    'import/resolver': {
+      'babel-plugin-root-import': {
+        rootPathSuffix: 'src',
       },
-      "ecmaVersion": 2018,
-      "sourceType": "module"
+    },
   },
-  "plugins": [
-      "react",
-      "prettier",
-      "react-hooks"
-  ],
-  "rules": {
-    "prettier/prettier": "error",
-    "react/jsx-filename-extension": [
-      'warn',
-      { extensions: ['.jsx', '.js'] }
-    ],
-    "import/prefer-default-export": "off",
-    "no-param-reassign": "off",
-    "camelcase": "off",
-    "no-console": ["error", {allow: ["tron"]}],
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn"
-  },
-  "settings": {
-    "import/resolver": {
-      "babel-plugin-root-import": {
-        rootPathSuffix: "src"
-      }
-    }
-  }
 };
